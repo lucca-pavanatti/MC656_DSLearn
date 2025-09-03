61656cffd706bb6bf5fd7564a3f3368f0b8148ba
lucca-pavanatti <luccapduarte@gmail.com>
2025-09-03 13:21:21 -0300

    criação da estrutura inicial do backend

---
+++ backend/dslearn/src/main/java/com/mc656/dslearn/DsLearnApplication.java
@@ -0,0 +1,13 @@
+package com.mc656.dslearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsLearnApplication.class, args);
+	}
+
+}
---
+++ backend/dslearn/src/test/java/com/mc656/dslearn/DsLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mc656.dslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
