3319fca83835d6dc56fcb10e81ec7337784ede42
lucca-pavanatti <luccapduarte@gmail.com>
2025-09-07 14:05:58 -0300

    Skip teste desnecessário

--- backend/dslearn/src/test/java/com/mc656/dslearn/DsLearnApplicationTests.java
+++ backend/dslearn/src/test/java/com/mc656/dslearn/DsLearnApplicationTests.java
@@ -1,13 +1,15 @@
 package com.mc656.dslearn;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-//class DsLearnApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+@SpringBootTest
+class DsLearnApplicationTests {
+
+	@Disabled
+	@Test
+	void contextLoads() {
+	}
+
+}
