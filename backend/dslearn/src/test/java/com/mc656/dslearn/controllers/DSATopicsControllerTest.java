46cd98dbe9c002390a40fe74a7139fd584ac2946
VctorMoraes - v194773 <v194773@dac.unicamp.br>
2025-11-06 19:04:25 -0300

    Remoção do @Autowired e utilização de Construtores

--- backend/dslearn/src/test/java/com/mc656/dslearn/controllers/DSATopicsControllerTest.java
+++ backend/dslearn/src/test/java/com/mc656/dslearn/controllers/DSATopicsControllerTest.java
@@ -8,12 +8,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
--- backend/dslearn/src/test/java/com/mc656/dslearn/controllers/ExercisesControllerTest.java
+++ backend/dslearn/src/test/java/com/mc656/dslearn/controllers/ExercisesControllerTest.java
@@ -5,6 +5,7 @@ import com.mc656.dslearn.dtos.PagedResponseDTO;
 import com.mc656.dslearn.services.ExercisesService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -18,7 +19,6 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(MockitoExtension.class)
 public class ExercisesControllerTest {
