da7c62f1d4eb4e32bdc1332b8626fd6a8101cb46
denisesot <74378374+denisesot@users.noreply.github.com>
2025-11-06 11:28:28 -0300

    Change SearchRepository to a fun interface

--- app/DSLearn/app/src/main/java/com/unicamp/dslearn/data/repository/SearchRepository.kt
+++ app/DSLearn/app/src/main/java/com/unicamp/dslearn/data/repository/SearchRepository.kt
@@ -4,7 +4,7 @@ import androidx.paging.PagingData
 import com.unicamp.dslearn.core.model.CardModel
 import kotlinx.coroutines.flow.Flow
 
-interface SearchRepository {
+fun interface SearchRepository {
 
     fun searchByQuery(query: String): Flow<PagingData<CardModel>>
 
